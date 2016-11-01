b86b9581ae1bc18796ccd67d0ffa54f4f3ad3e5f
cvvmusic <cvvofficial@gmail.com>
2016-11-01 17:08:07 +0300

    commit

--- scripts/vkInit.js
+++ scripts/vkInit.js
@@ -5,10 +5,13 @@ var userId = 0;
 
 VK.init(function() {
 
-	VK.api("users.get", {"fields": "first_name"}, function (data) {
+	VK.api("users.get", {"fields": "photo_200"}, function (data) {
 
+		userAvatar.setImage(data.response['0'].photo_200);
     	userFirstName.setText(data.response['0'].first_name);
-
+    	userLastName.setText(data.response['0'].last_name);
+    	userId.setText(data.response['0'].id);
+    	
 	});
     
 }, function() {
