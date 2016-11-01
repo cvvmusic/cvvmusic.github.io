ff716300e92d17b25c10846192eb923ae2b95f62
cvvmusic <cvvofficial@gmail.com>
2016-11-01 17:36:14 +0300

    com

--- scripts/vkInit.js
+++ scripts/vkInit.js
@@ -1,15 +1,15 @@
 var userAvatar;
-var userFirstName;
-var userLastName;
-var userId;
+var userFirstName = "";
+var userLastName = "";
+var userId = "";
 
 VK.init(function() {
 
 	VK.api("users.get", {"fields": "photo_200"}, function (data) {
 
-    	userFirstName = data.response['0'].first_name;
-    	userLastName = data.response['0'].last_name;
-    	userId = data.response['0'].id;
+    	userFirstName.setText(data.response['0'].first_name);
+    	userLastName.setText(data.response['0'].last_name);
+    	userId.setText(data.response['0'].id);
 
 	});
     
